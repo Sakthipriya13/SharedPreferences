8fa876a189cea8398e1c2067ff6b2f57f2cec51b
SAKTHIPRIYA <sakthipriyaravi2019@gmail.com>
2025-10-17 12:59:54 +0530

    Commit

--- app/src/main/java/com/example/sharedpreferenceapplication/ui/Login.kt
+++ app/src/main/java/com/example/sharedpreferenceapplication/ui/Login.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.sharedpreferenceapplication.R
 import com.example.sharedpreferenceapplication.databinding.ActivityMainBinding
 import com.example.sharedpreferenceapplication.databinding.LoginBinding
@@ -41,7 +42,10 @@ class Login : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.login, container, false)
+//        return inflater.inflate(R.layout.login, container, false)
+        binding=DataBindingUtil.inflate(inflater,R.layout.login,container,false)
+        binding.lifecycleOwner=this
+        return  binding.root
     }
 
     companion object {
